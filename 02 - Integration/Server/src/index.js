0bf464563d92da23589be5ea4b80fcf7c443edef
Vale51 <102689997+Vale51@users.noreply.github.com>
2023-08-05 04:26:09 -0300

    promises working

--- 02 - Integration/Server/src/index.js
+++ 02 - Integration/Server/src/index.js
@@ -1,39 +1,29 @@
 const { log } = require("console");
-const data = require("./utils/data");
+const getCharById = require("./controllers/getCharById")
+
+const cors = require("cors"); // Importa el paquete cors
 const http = require("http");
 
-http.createServer((req, res) => {
+const app = http.createServer((req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
   if (req.url.includes("/rickandmorty/character")) {
     const id = req.url.split("/").pop();
-    // console.log(data);
-    
-    
-    for (const char of data) {
-        
-       
-            //string //int
-        if (+id === char.id) {
-            
-            
-            res.writeHead(200, { "Content-type": "application/json" });
-            res.end(JSON.stringify(char));
-            return;
-        }
-      }
-      
-
-    // If the character is not found, send a 404 response
-    res.writeHead(404, { "Content-type": "text/plain" });
-    res.end("Character not found.");
+    getCharById(res, id);
   } else {
-    // If the URL doesn't match, send a 404 response
-    res.writeHead(404, { "Content-type": "text/plain" });
-    res.end("Page not found.");
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Ruta no encontrada');
   }
+  
+
+
 })
 
-.listen(3001, () => {
+
+
+
+app.listen(3001, () => {
   console.log('Server is running on port 3001');
 });
